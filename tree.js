9a86547d170fda37b7fa1f1dad7b392484cee4b6
OptimisticTrousers <tony.isern@yahoo.com>
2022-09-13 07:52:11 -0400

    Using the initialized array to build the tree

--- tree.js
+++ tree.js
@@ -16,12 +16,12 @@ class Tree {
     }
   };
 
-  buildTree = (array) => {
-    mergeSort(array);
-    removeDuplicates(array);
-    const n = array.length - 1;
+  buildTree = () => {
+    mergeSort(this.array);
+    removeDuplicates(this.array);
+    const n = this.array.length - 1;
     const mid = n / 2;
-    this.root = new Node(array[mid]);
+    this.root = new Node(this.array[mid]);
     this.root.left = this.buildTree(array.slice(0, mid - 1));
     this.root.right = this.buildTree(array.slice(mid + 1, n));
     return this.root;
