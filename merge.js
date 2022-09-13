6ee9a0a63b9633878e26e3b5636e7493ad82ffa4
OptimisticTrousers <tony.isern@yahoo.com>
2022-09-13 07:13:54 -0400

    Writing code for merge sort

---
+++ merge.js
@@ -0,0 +1,45 @@
+function mergeSort(array) {
+  const n = array.length;
+
+  if (n < 2) {
+    return array;
+  }
+
+  const firstHalf = array.slice(0, n / 2);
+  const secondHalf = array.slice(n / 2);
+
+  mergeSort(firstHalf);
+  mergeSort(secondHalf);
+  merge(array, firstHalf, secondHalf);
+}
+
+function merge(array, firstHalf, secondHalf) {
+  let leftIndex = 0;
+  let rightIndex = 0;
+  let arrayIndex = 0;
+
+  while (leftIndex < firstHalf.length && rightIndex < secondHalf.length) {
+    if (firstHalf[leftIndex] < secondHalf[rightIndex]) {
+      array[arrayIndex] = firstHalf[leftIndex];
+      leftIndex++;
+    } else {
+      array[arrayIndex] = secondHalf[rightIndex];
+      rightIndex++;
+    }
+    arrayIndex++;
+  }
+
+  while (leftIndex < firstHalf.length) {
+    array[arrayIndex] = firstHalf[leftIndex];
+    leftIndex++;
+    arrayIndex++;
+  }
+
+  while (rightIndex < secondHalf.length) {
+    array[arrayIndex] = secondHalf[rightIndex];
+    rightIndex++;
+    arrayIndex++;
+  }
+}
+
+module.exports = mergeSort;
--- tree.js
+++ tree.js
@@ -7,9 +7,12 @@ class Tree {
   buildTree = (array) => {
     const n = array.length - 1;
     const mid = n / 2;
-    const newNode = new Node(array[mid]);
-    this.root = newNode;
+    this.root = new Node(array[mid]);
+    this.root.left = this.buildTree(array.slice(0, mid - 1));
+    this.root.right = this.buildTree(array.slice(mid + 1, n));
+    return this.root;
   };
 }
+
 
 module.exports = Tree;
