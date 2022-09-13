88d6ae83de7daee763c8b8b0e6ffff9255f8b269
OptimisticTrousers <tony.isern@yahoo.com>
2022-09-13 06:11:14 -0400

    Writing buildTree func

--- tree.js
+++ tree.js
@@ -1,6 +1,13 @@
+const Node = require("./node");
 class Tree {
   constructor(array) {
     this.array = [...array];
     this.root = null;
   }
+  buildTree = (array) => {
+    const n = array.length - 1;
+    const mid = n / 2;
+    const newNode = new Node(array[mid]);
+    this.root = newNode;
+  };
 }
